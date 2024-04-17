3c7c7d0060224aedf71197794fdfb5dad30d56ff
karim <haddadi.karim20@gmail.com>
2024-04-17 15:44:40 +0200

    fix : Add TripView component and update router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TripView from '@/views/TripView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,15 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+
+    {
+
+
+      path: '/trips/:id',
+      name: 'trips',
+      component: TripView
+
+    }
  
   ]
 })
